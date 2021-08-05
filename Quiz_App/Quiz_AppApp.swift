b5f504f36700c83912108d9bb2f7a48660cf4600
Vithushan <lvithu123@outlook.com>
2021-08-05 10:44:46 +0530

    Initial Commit

---
+++ Quiz_App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz_App
+//
+//  Created by Vithushan Logan on 2021-08-05.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
